c90f273d0a30afb58f77f37998cd31dd06d72bba
Snehal979 <bansodsnehal535@gmail.com>
2022-12-12 07:26:56 +0530

    (snehal)Add:file main program

---
+++ FileIOandStream/FileIOandStream/FileData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOandStream
+{
+    public class FileData
+    {
+        public static void FileExist()
+        {
+            string filepath = "C:\\Users\\hp\\Desktop\\newFolder\\FileIOAndStreamDemo\\FileIOandStream\\FileIOandStream\\Example.txt";
+            if (File.Exists(filepath))
+            {
+                Console.WriteLine("File Exists");
+            }
+        }
+    }
+}
---
+++ FileIOandStream/FileIOandStream/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FileIOandStream
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            FileData.FileExist();
+        }
+    }
+}
